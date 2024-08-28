c28276eef9daeadab3d705b54a0b8c2fc72cf2b5
Maorizio <caplanmaor@gmail.com>
2024-08-28 13:59:30 +0300

    rename var for better code clarity

--- server/seed.py
+++ server/seed.py
@@ -9,7 +9,7 @@ salt = bcrypt.gensalt()
 async def seed_database():
     username = os.getenv('SEED_USER', 'default_username')
     password = os.getenv('SEED_PASSWORD', 'default_password')
-    password = bcrypt.hashpw(password.encode('utf-8'), salt).decode('utf-8')
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt).decode('utf-8')
 
     query = select(user_table.c.username).where(user_table.c.username == username)
     existing_user = await database.fetch_one(query)
@@ -17,6 +17,6 @@ async def seed_database():
     if existing_user:
         print(f"User {username} already exists, seeding skipped.")
     else:
-        insert_query = insert(user_table).values(username=username, password=password)
+        insert_query = insert(user_table).values(username=username, password=hashed_password)
         await database.execute(insert_query)
         print(f"User {username} seeded successfully.")
