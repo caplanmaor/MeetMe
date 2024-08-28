c1a30cba0c97cf02e148ffda5c13aca5dccffd0a
Maorizio <caplanmaor@gmail.com>
2024-08-28 15:47:15 +0300

    make the seed function add 2 users

--- server/seed.py
+++ server/seed.py
@@ -7,16 +7,25 @@ import bcrypt
 salt = bcrypt.gensalt()
 
 async def seed_database():
-    username = os.getenv('SEED_USER', 'default_username')
-    password = os.getenv('SEED_PASSWORD', 'default_password')
+    username_1 = os.getenv('SEED_USER_1', 'user1')
+    username_2 = os.getenv('SEED_USER_2', 'user2')
+    password = os.getenv('SEED_PASSWORD', '1234')
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt).decode('utf-8')
 
-    query = select(user_table.c.username).where(user_table.c.username == username)
-    existing_user = await database.fetch_one(query)
+    existing_user_1 = await database.fetch_one(select(user_table.c.username).where(user_table.c.username == username_1))
+    existing_user_2 = await database.fetch_one(select(user_table.c.username).where(user_table.c.username == username_2))
 
-    if existing_user:
-        print(f"User {username} already exists, seeding skipped.")
+    if existing_user_1:
+        print(f"User {username_1} already exists, seeding skipped.")
     else:
-        insert_query = insert(user_table).values(username=username, password=hashed_password)
+        insert_query = insert(user_table).values(username=username_1, password=hashed_password)
         await database.execute(insert_query)
-        print(f"User {username} seeded successfully.")
+        print(f"User {username_1} seeded successfully.")
+    
+    if existing_user_2:
+        print(f"User {username_2} already exists, seeding skipped.")
+    else:
+        insert_query = insert(user_table).values(username=username_2, password=hashed_password)
+        await database.execute(insert_query)
+        print(f"User {username_2} seeded successfully.")
+
